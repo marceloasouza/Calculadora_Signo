53663ff543a2f208edecba0536b7225de1980239
Marcelo Souza <marceloamorsouza@yahoo.com.br>
2018-07-18 15:58:35 -0300

    Calculadora de signo em c#

---
+++ Calculadora_Signo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Signo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia;
+            string mes;
+
+            Console.WriteLine("Em que dia você nasceu?");
+            dia = Console.ReadLine();
+
+            Console.WriteLine("Em que mes você nasceu?");
+            mes = Console.ReadLine();
+
+            int diaInt = 0;
+            int mesInt = 0;
+
+            try
+            {
+                diaInt = Convert.ToInt32(dia);
+                mesInt = Convert.ToInt32(mes);
+            }
+            catch (FormatException ex)
+            {
+
+               Console.WriteLine(ex.Message);
+                
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+
+
+            InterpregadorSigno interpretador = new InterpregadorSigno();
+            Signo signo = interpretador.Interpretar(diaInt, mesInt );
+
+            if(signo != null)
+            {
+                Console.WriteLine("Seu signo é " + signo.nome + " -- " + signo.caracteristicas);
+                
+            }
+            else
+            {
+                Console.WriteLine("Dia ou mês invalido.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Calculadora_Signo/Signo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Signo
+{
+    class Signo
+    {
+        public string nome;
+        public int diaInicio;
+        public int mesInicio;
+        public int diaFim;
+        public int mesFim;
+        public string caracteristicas;
+    }
+}
